633fe9bb60f92bec01311387918bde49968d1796
yash sawarkar <yashsawarkar01@gmail.com>
2024-08-30 15:11:38 +0530

    minor bugs are fixed

--- src/pages/Channel/ChannelTweets.jsx
+++ src/pages/Channel/ChannelTweets.jsx
@@ -4,32 +4,32 @@ import { getUserTweets } from "../../store/Slices/tweetSlice";
 import { TweetAndComment, TweetsList } from "../../components";
 
 function ChannelTweets() {
-    const dispatch = useDispatch();
-    const authId = useSelector((state) => state.auth?.userData?._id);
-    const userId = useSelector((state) => state.user?.profileData?._id);
-    const tweets = useSelector((state) => state.tweet?.tweets);
+  const dispatch = useDispatch();
+  const authId = useSelector((state) => state.auth?.userData?._id);
+  const userId = useSelector((state) => state.user?.profileData?._id);
+  const tweets = useSelector((state) => state.tweet?.tweets);
 
-    useEffect(() => {
-        if (userId) dispatch(getUserTweets(userId));
-    }, [dispatch, userId]);
+  useEffect(() => {
+    if (userId) dispatch(getUserTweets(userId));
+  }, [dispatch, userId]);
 
-    return (
-        <>
-            {authId === userId && <TweetAndComment tweet={true}/>}
-            {tweets?.map((tweet) => (
-                <TweetsList
-                    key={tweet?._id}
-                    avatar={tweet?.ownerDetails?.avatar.url}
-                    content={tweet?.content}
-                    createdAt={tweet?.createdAt}
-                    likesCount={tweet?.likesCount}
-                    tweetId={tweet?._id}
-                    username={tweet?.ownerDetails?.username}
-                    isLiked={tweet?.isLiked}
-                />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {authId === userId && <TweetAndComment tweet={true} />}
+      {tweets?.map((tweet) => (
+        <TweetsList
+          key={tweet?._id}
+          avatar={tweet?.ownerDetails?.avatar.url}
+          content={tweet?.content}
+          createdAt={tweet?.createdAt}
+          likesCount={tweet?.likesCount}
+          tweetId={tweet?._id}
+          username={tweet?.ownerDetails?.username}
+          isLiked={tweet?.isLiked}
+        />
+      ))}
+    </>
+  );
 }
 
 export default ChannelTweets;
